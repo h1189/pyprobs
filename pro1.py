82e2af42d155a8f8589da4ee28d7b25dfd739386
h1189 <sunkireddyh@gmail.com>
2020-12-10 19:40:16 +0530

    Added Program description

--- pro1.py
+++ pro1.py
@@ -1,3 +1,5 @@
+'''Create a program that asks the user to enter their name and their age.
+ Print out a message addressed to them that tells them the year that they will turn 100 years old.'''
 name = str(input('enter the name of user:'))
 age = int(input('enter age:'))
 cyear = int(input('mention current year:'))
--- pro2.py
+++ pro2.py
@@ -1,3 +1,5 @@
+#Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.
+#If the number is a multiple of 4, print out a different message.
 num = int(input('enter a num:'))
 if num%2 == 0:
    print(num,' is an even num')
