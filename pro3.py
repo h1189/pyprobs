f0f3f5a99cdac82cc674e4b48baa2992ccac9500
h1189 <sunkireddyh@gmail.com>
2020-12-11 15:42:29 +0530

    updated program

--- pro3.py
+++ pro3.py
@@ -1,8 +1,9 @@
 #write a program that prints out all the elements of the list that are less than 5.
 a = [23, 4, 5, 7, 6, 2, 1, 9, 8, 32, 12, 45]
-print(a)
+x = []
 for element in a :
     if element <= 5:
-       print (element)
+       x.append(element)
+print(x)
     
 
