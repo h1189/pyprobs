7202a8d287c7897840ba815ee0dc94bf09e6c794
h1189 <sunkireddyh@gmail.com>
2020-12-11 15:45:06 +0530

    comment changed

--- pro3.py
+++ pro3.py
@@ -1,4 +1,4 @@
-#write a program that prints out all the elements of the list that are less than 5.
+#write a program that prints out all the elements of the list that are less than 5 in an new list.
 a = [23, 4, 5, 7, 6, 2, 1, 9, 8, 32, 12, 45]
 x = []
 for element in a :
