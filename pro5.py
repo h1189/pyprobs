914c79abddd8bc4f6e6a195575e4740bdc26e056
h1189 <sunkireddyh@gmail.com>
2020-12-13 23:35:27 +0530

    program 5

--- pro5.py
+++ pro5.py
@@ -1,4 +1,4 @@
-'''Take two listsand write a program that returns a list that contains only the elements that are common between 
+'''Take two lists and write a program that returns a list that contains only the elements that are common between 
    the lists (without duplicates). Make sure your program works on two lists of different sizes.''' 
 a = [2, 4, 5, 3, 6, 7, 4]
 b = [2, 3, 5, 9, 7, 0, 19, 11] 
