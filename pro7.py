360c88b513d015ecdd0265439858cb0907cd2ffe
h1189 <sunkireddyh@gmail.com>
2020-12-14 21:02:14 +0530

    program 7

---
+++ pro7.py
@@ -0,0 +1,22 @@
+'''Write a function called fizz_buzz that takes a number.
+If the number is divisible by 3, it should return “Fizz”.
+If it is divisible by 5, it should return “Buzz”.
+If it is divisible by both 3 and 5, it should return “FizzBuzz”.
+Otherwise, it should return the same number.'''
+
+def fizz_buzz(x):
+    if x % 3 == 0 and x % 5 == 0:
+        y = "Fizz_Buzz"
+        return y
+    elif x % 3 == 0:
+        y = "Fizz"
+        return y
+    elif x % 5 == 0:
+        y = "Buzz"
+        return y
+    else:
+        return x
+
+c = fizz_buzz(9) 
+print(c)
+
