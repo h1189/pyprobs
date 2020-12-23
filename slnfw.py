94a8a1ab94086fbfda6a6ae58669a73fd6fdabe4
h1189 <sunkireddyh@gmail.com>
2020-12-23 17:02:00 +0530

    SLN functions updated

--- slnfw.py
+++ slnfw.py
@@ -1,47 +1,60 @@
 import sys
 
+def read_user_input(msg, expected_input_type):
+    user_input = input(msg)
+    if expected_input_type == "int":
+        try:
+            return int(user_input)
+        except ValueError:
+            print('Invalid choice, Enter a valid choice....')
+            read_user_input(msg, expected_input_type)
+    elif expected_input_type == "float":
+        try:
+            return float(user_input)
+        except ValueError:
+            print('Invalid choice, Enter a valid choice....')
+            read_user_input(msg, expected_input_type)
+    elif expected_input_type == "str":
+        try:
+            return str(user_input)
+        except ValueError:
+            print('Invalid Name, Enter an valid Name...')
+            read_user_input(msg, expected_input_type)
+    else:
+        print("Wrong input type ")
+        read_user_input(msg, expected_input_type)
+
+
 # check availability of raw material
 
 def raw_material_check():
-    try:
-        ava = int(input('is RAW MATERIAL available ? '))
-        while ava == 1:
-            print('RAW MATERIAL AVAILABLE \n Ready to take orders..') 
-            break
-        else:
-            print('OUT OF RAW MATERIAL..\n Place an order')
-            sys.exit()
-    except ValueError:
-        print('Invalid choice, Enter a valid choice....')
-        sys.exit()
-    
-raw_material_check()
- 
+    print("#TODO implement raw material check functionality")
+
 # Customer Needs
 
 def customer_needs():
     type_of_materials_available = {1: 'Dimond fencing', 2: 'Barbered wire fencing', 3:'Chain link fencing', 4: 'Iron Poles'}
     print(f'Available Product types are {type_of_materials_available}')
-    try:
-        choice = int(input('Select the product needed:'))
-        for key in type_of_materials_available:
-            if key == choice:
-                print(f'You have selected {type_of_materials_available[key]}')
-                break
-        else:
-            print('Choice not found. Please select an valid choice')
-            sys.exit()
-        
-    except ValueError:
-        print('Invalid choice, Please select valid choice..')
-        sys.exit()
-    try:
-        quantity = float(input(f"Mention Qunatity of {type_of_materials_available[key]} needed in KG's : "))
-        print(f"Make an order of {quantity} KG's of {type_of_materials_available[key]} and \n Process the Order")
-    except ValueError:
-        print("You have mentioned wrong data...Please mention the quantity in KG's ")
-        sys.exit()
+    choice = read_user_input('Select the product needed:', "int")
+    if choice in type_of_materials_available:
+        print(f'You have selected {type_of_materials_available[choice]}')
+    else:
+        print('Choice not found. Please select an valid choice')
+        customer_needs()
+    qty_msg = "Mention Qunatity of " + type_of_materials_available[choice] + " needed in KG's : "
+    quantity = read_user_input(qty_msg, "float")
+    print(f"Make an order of {quantity} KG's of {type_of_materials_available[choice]} and \n Process the Order")
 
-customer_needs()
+
 
 # Customer Details
+
+def customer_details():
+        name_of_customer = read_user_input('Enter Name of the customer: ','str')
+        contact_number = read_user_input('Enter the contact number: ','int')
+        customer_address = read_user_input('Address of the customer: ','str')
+
+def init():
+    raw_material_check()
+    customer_needs()
+    customer_details()
