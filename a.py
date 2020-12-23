441920b4a8cc7c60ae3eb082d77ec3d914629eef
h1189 <sunkireddyh@gmail.com>
2020-12-23 17:03:52 +0530

    changes in program code

--- a.py
+++ a.py
@@ -1,8 +1,7 @@
-# Given a list of numbers, return True if first and last number of a list is same
+#XXX: # Given a list of numbers, return True if first and last number of a list is same
 
 a = [1, 3, 4, 2, 5, 9, 1]
 if a[0]== a[-1]:
     print(f'given list {a} result is true')
 else:
     print(f'given list {a} result is false')
-
